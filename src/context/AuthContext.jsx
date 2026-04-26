03340da15d159ea96135121361bfe96f16cc114f
Mark Gerald Pacun <markgpacun@gmail.com>
2026-04-26 17:12:58 +0800

    Initial Commit - New Files

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,37 +3,37 @@ import { getMe } from '../api';
 
 const AuthContext = createContext();
 
-export function AuthProvider({children}) {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            getMe()
-                .then(res => setUser(res.data))
-                .catch(() => localStorage.removeItem('token'))
-                .finally(() => setLoading(false));
-        } else {
-            setLoading(false);
-        }
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      getMe()
+        .then(res => setUser(res.data))
+        .catch(() => localStorage.removeItem('token'))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, []);
 
-    const login = (token, userData) => {
-        localStorage.setItem('token', token)
-        setUser(userData);
-    };
+  const login = (token, userData) => {
+    localStorage.setItem('token', token);
+    setUser(userData);
+  };
 
-    const logout = () => {
-        localStorage.removeItem('token');
-        setUser(null);
-    };
+  const logout = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
 
-    return (
-        <AuthContext.Provider value = {{user, login, logout, loading}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export const useAuth = () => useContext(AuthContext);
